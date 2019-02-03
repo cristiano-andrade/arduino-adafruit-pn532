963faea390f79eac945836c0801ab2f860c406f4
Cristiano Andrade <cristiano.s.andrade@gmail.com>
2019-02-03 19:55:03 -0200

    readNTag2xx

--- src/mgos_arduino_adafruit_pn532.cpp
+++ src/mgos_arduino_adafruit_pn532.cpp
@@ -4,33 +4,72 @@ Adafruit_PN532 *mgos_arduino_adafruit_pn532_create(uint8_t clk, uint8_t miso, ui
     return new Adafruit_PN532(clk, miso, mosi, ss);
 }
 
-int mgos_arduino_adafruit_pn532_begin(Adafruit_PN532 * htu) {
-    if (htu == nullptr) return 0;
-    htu->begin();
+int mgos_arduino_adafruit_pn532_begin(Adafruit_PN532 * nfc) {
+    if (nfc == nullptr) return 0;
+    nfc->begin();
     return 1;
 }
 
 
-int mgos_arduino_adafruit_pn532_getFirmwareVersion(Adafruit_PN532 * htu) {
+int mgos_arduino_adafruit_pn532_getFirmwareVersion(Adafruit_PN532 * nfc) {
   int response = -1;
 
-  if (htu != nullptr) {
-    response = (int) htu->getFirmwareVersion();
+  if (nfc != nullptr) {
+    response = (int) nfc->getFirmwareVersion();
   }
 
   return response;
 }
 
-bool mgos_arduino_adafruit_pn532_readPassiveTargetID(Adafruit_PN532 * htu,
+bool mgos_arduino_adafruit_pn532_readPassiveTargetID(Adafruit_PN532 * nfc,
                                                      uint8_t cardbaudrate,
                                                      uint8_t * uid,
                                                      uint8_t * uidLength,
                                                      uint16_t timeout = 0) {
   bool success = false;
 
-  if(htu != nullptr) {
-    success = htu->readPassiveTargetID(cardbaudrate, uid, uidLength, timeout);
+  if(nfc != nullptr) {
+    success = nfc->readPassiveTargetID(cardbaudrate, uid, uidLength, timeout);
   }
 
   return success;
+}
+
+uint8_t mgos_arduino_adafruit_pn532_ntag2xx_ReadPage(Adafruit_PN532 * nfc,
+                                                     uint8_t page,
+                                                     uint8_t * buffer) {
+
+   uint8_t response = 0;
+
+   if(nfc != nullptr) {
+     response = nfc->ntag2xx_ReadPage(page,buffer);
+   }
+
+   return response;
+
+}
+
+uint8_t mgos_arduino_adafruit_pn532_ntag2xx_WritePage(Adafruit_PN532 * nfc,
+                                                      uint8_t page,
+                                                      uint8_t * data) {
+  uint8_t response = 0;
+
+  if(nfc != nullptr) {
+    response = nfc->ntag2xx_WritePage(page,data);
+  }
+
+  return response;
+}
+
+
+uint8_t mgos_arduino_adafruit_pn532_ntag2xx_WriteNDEFURI(Adafruit_PN532 * nfc,
+                                                         uint8_t uriIdentifier,
+                                                         char * url, uint8_t dataLen) {
+   uint8_t response = 0;
+
+   if(nfc != nullptr) {
+     response = nfc->ntag2xx_WriteNDEFURI(uriIdentifier,url,dataLen);
+   }
+
+   return response;
 }
--- src/mgos_arduino_adafruit_pn532.h
+++ src/mgos_arduino_adafruit_pn532.h
@@ -8,16 +8,28 @@ extern "C" {
 #endif
 
 Adafruit_PN532 *mgos_arduino_adafruit_pn532_create(uint8_t clk, uint8_t miso, uint8_t mosi, uint8_t ss);
-int mgos_arduino_adafruit_pn532_begin(Adafruit_PN532 *htu);
-int mgos_arduino_adafruit_pn532_getFirmwareVersion(Adafruit_PN532 *htu);
-bool mgos_arduino_adafruit_pn532_readPassiveTargetID(Adafruit_PN532 * htu,
+int mgos_arduino_adafruit_pn532_begin(Adafruit_PN532 *nfc);
+int mgos_arduino_adafruit_pn532_getFirmwareVersion(Adafruit_PN532 *nfc);
+bool mgos_arduino_adafruit_pn532_readPassiveTargetID(Adafruit_PN532 * nfc,
                                                      uint8_t cardbaudrate,
                                                      uint8_t * uid,
                                                      uint8_t * uidLength,
                                                      uint16_t timeout = 0);
 
+uint8_t mgos_arduino_adafruit_pn532_ntag2xx_ReadPage(Adafruit_PN532 * nfc,
+                                                     uint8_t page,
+                                                     uint8_t * buffer);
+
+uint8_t mgos_arduino_adafruit_pn532_ntag2xx_WritePage(Adafruit_PN532 * nfc,
+                                                      uint8_t page,
+                                                      uint8_t * data);
+
+uint8_t mgos_arduino_adafruit_pn532_ntag2xx_WriteNDEFURI(Adafruit_PN532 * nfc,
+                                                         uint8_t uriIdentifier,
+                                                         char * url, uint8_t dataLen);
+
 #ifdef __cplusplus
 }
 #endif
 
-#endif //ARDUINO_ADAFRUIT_HTUD1DF_MGOS_ARDUINO_ADAFRUIT_HTU21D_C_H
+#endif //ARDUINO_PN532_MGOS_ARDUINO_ADAFRUIT_PN532_C_H
