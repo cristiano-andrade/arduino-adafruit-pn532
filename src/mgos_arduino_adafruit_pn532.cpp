727841ec747235fe9d0d0ce82f4967272abde562
Cristiano Andrade <cristiano.s.andrade@gmail.com>
2019-02-03 17:46:02 -0200

    readPassiveTargetID ported

--- src/mgos_arduino_adafruit_pn532.cpp
+++ src/mgos_arduino_adafruit_pn532.cpp
@@ -4,14 +4,14 @@ Adafruit_PN532 *mgos_arduino_adafruit_pn532_create(uint8_t clk, uint8_t miso, ui
     return new Adafruit_PN532(clk, miso, mosi, ss);
 }
 
-int mgos_arduino_adafruit_pn532_begin(Adafruit_PN532 *htu) {
+int mgos_arduino_adafruit_pn532_begin(Adafruit_PN532 * htu) {
     if (htu == nullptr) return 0;
     htu->begin();
     return 1;
 }
 
 
-int mgos_arduino_adafruit_pn532_getFirmwareVersion(Adafruit_PN532 *htu) {
+int mgos_arduino_adafruit_pn532_getFirmwareVersion(Adafruit_PN532 * htu) {
   int response = -1;
 
   if (htu != nullptr) {
@@ -19,4 +19,18 @@ int mgos_arduino_adafruit_pn532_getFirmwareVersion(Adafruit_PN532 *htu) {
   }
 
   return response;
+}
+
+bool mgos_arduino_adafruit_pn532_readPassiveTargetID(Adafruit_PN532 * htu,
+                                                     uint8_t cardbaudrate,
+                                                     uint8_t * uid,
+                                                     uint8_t * uidLength,
+                                                     optional int timeout = 0) {
+  bool success = false;
+
+  if(htu != nullptr) {
+    success = htu->readPassiveTargetID(cardbaudrate, uid, uidLength, timeout);
+  }
+
+  return success;
 }
